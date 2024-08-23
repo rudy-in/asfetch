6569ae61f7a314a7b0f2e0df4a9c32c66b7ddbff
rudy-in <msharmion@gmail.com>
2024-08-23 20:30:51 +0530

    Make CPU detection dynamic

--- src/components/cpu.asm
+++ src/components/cpu.asm
@@ -2,26 +2,34 @@
 
 section .data
     cpu_msg db "CPU: ", 0
+    lscpu_cmd db "/usr/bin/lscpu", 0  ; Path to the lscpu command
+    lscpu_args db "", 0              ; No arguments for lscpu in this example
+    stdout db 1                      ; File descriptor for stdout
+    buffer_size equ 256
+    newline db 0xA                  ; Newline character
 
 section .bss
-    cpu_info resb 256
+    buffer resb buffer_size         ; Reserve space for command output
 
 section .text
     global fetch_cpu
-    fetch_cpu:
-        ; Command to fetch CPU information (using `lscpu`)
-        mov rax, 0x01       ; syscall number for sys_write
-        mov rdi, 0x01       ; file descriptor 1 is stdout
-        mov rsi , cpu_msg
-        mov rdx, 5          ; length of cpu_msg
-        syscall
 
-        ; Use a fixed string for simplicity
-        mov rax, 0x01       ; syscall number for sys_write
-        mov rdi, 0x01       ; file descriptor 1 is stdout
-        long_string db "11th Gen Intel i5-1135G7 (8) @ 4.200GHz", 0
-        mov rdx, 35         ; length of CPU string
-        syscall
+fetch_cpu:
+    ; Print the CPU message
+    mov rax, 1                      ; syscall number for sys_write
+    mov rdi, 1                      ; file descriptor 1 is stdout
+    lea rsi, [cpu_msg]
+    mov rdx, 5                      ; length of cpu_msg
+    syscall
 
-        ; Return from function
-        ret
+    ; Prepare to execute lscpu
+    mov rax, 59                     ; syscall number for sys_execve
+    lea rdi, [lscpu_cmd]           ; path to lscpu
+    lea rsi, [lscpu_args]          ; arguments (empty in this case)
+    xor rdx, rdx                    ; environment (not used)
+    syscall
+
+    ; Check if execve succeeded
+    ; If execve fails, control will return here. For this example, we'll just return.
+    ; Normally, you would handle errors and process output more carefully.
+    ret
